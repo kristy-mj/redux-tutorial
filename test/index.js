1531f57dc400f42fdc32356bfd1e543cb280ec57
Rena Goldsmith <rena.kg@gmail.com>
2016-07-11 16:02:32 +1200

    tests NOT PASSING lol

--- test/index.js
+++ test/index.js
@@ -2,26 +2,33 @@ var test = require('tape')
 var expect = require('expect')
 var deepFreeze = require('deep-freeze')
 
+const todo = (state, action) => {
+    switch (action.type) {
+        case 'ADD_TODO':
+            return {
+                id: action.id,
+                text: action.text,
+                completed: false
+            }
+        case 'TOGGLE_TODO':
+            if (state.id !== action.id) {
+                return state
+            }
+            return Object.assign({}, state, {completed: !state.completed})
+        default:
+            return state
+    }
+}
 
 const todos = (state = [], action) => {
     switch (action.type) {
         case 'ADD_TODO':
             return [
                 ...state,
-                {
-                    id: action.id,
-                    text: action.text,
-                    completed: false
-                }
+                todo(undefined, action)
             ]
         case 'TOGGLE_TODO':
-            return state.map((todo) => {
-                if (todo.id !== action.id) {
-                    return todo
-                }
-
-                return Object.assign({}, todo, {completed: !todo.completed})
-            })
+            return state.map(t => todo(t, action))
         default:
             return state
     }
@@ -53,6 +60,7 @@ const testAddTodo = () => {
 }
 
 testAddTodo()
+console.log(testAddTodo())
 console.log('todos tests pass')
 
 //Test video 11?? could be 10
@@ -82,6 +90,7 @@ const testToggleTodo = () => {
 }
 
 testToggleTodo()
+console.log(testToggleTodo())
 console.log('toggleTodo tests pass')
 
 
@@ -127,5 +136,6 @@ const testToggleTodoTwo = () => {
 }
 
 testToggleTodoTwo()
+console.log(testToggleTodoTwo())
 console.log('toggleTodoTwo tests pass')
 
